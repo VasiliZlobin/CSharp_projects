0c4ae4abed64c98612f9e6dedc17cd78c0e89cf2
Vasili Zlobin <vaszlbn@gmail.com>
2022-01-30 09:59:43 +0300

    add Example 003 Sum and Div

---
+++ Example003_SumDiv/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(1, 101);
+int numberB = new Random().Next(1, 51);
+string temp = numberA.ToString() + " + " + numberB.ToString() + " = ";
+string resultSum = temp + (numberA + numberB);
+string resultDiv = temp.Replace("+", "/") + ((double)numberA / numberB);
+Console.WriteLine(resultSum);
+Console.WriteLine(resultDiv);
