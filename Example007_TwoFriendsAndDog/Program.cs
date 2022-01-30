5501b286127ca0539425e6f12b18f2b2f92036da
Vasili Zlobin <vaszlbn@gmail.com>
2022-01-30 14:08:13 +0300

    add Example 007 Two friends and dog

---
+++ Example007_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+bool isFirstFriendNext = false;
+
+while (distance > 10)
+{
+    int currentSpeed = secondFriendSpeed;
+    if (isFirstFriendNext)
+    {
+        currentSpeed = firstFriendSpeed;
+    }
+    double time = distance / (currentSpeed + dogSpeed);
+    distance = distance - time * (firstFriendSpeed + secondFriendSpeed);
+    count++;
+    Console.Write(count);
+    Console.Write(") осталось: ");
+    Console.WriteLine(distance);
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз");
