a1efa39af6347c5263cee53d4622d127b84d9cbd
Vasili Zlobin <vaszlbn@gmail.com>
2022-01-31 23:26:14 +0300

    change example 009 with array

--- Example009_DayOfWeek/Program.cs
+++ Example009_DayOfWeek/Program.cs
@@ -8,38 +8,14 @@ try
 catch (FormatException e)
 {}
 
+string[] names = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
 string result = "";
-
 if (numDay < 1 || numDay > 7)
 {
     result = "Нет такого дня недели!";
 }
-else if (numDay == 1)
-{
-    result = "Понедельник";
-}
-else if (numDay == 2)
-{
-    result = "Вторник";
-}
-else if (numDay == 3)
-{
-    result = "Среда";
-}
-else if (numDay == 4)
-{
-    result = "Четверг";
-}
-else if (numDay == 5)
-{
-    result = "Пятница";
-}
-else if (numDay == 6)
-{
-    result = "Суббота";
-}
 else
 {
-    result = "Воскресенье";
+    result = names[numDay - 1];
 }
 Console.WriteLine(result);
