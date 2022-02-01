70a8cedd0a1c4230224e3773955455356edd9eb2
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-01 13:17:20 +0300

    add solution task 8 even from 1 to N

---
+++ Task_8_Even_1-N/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = 0; 
+while (max < 1)
+{
+    Console.Write("Введите натуральное число, больше 1: ");
+    string input = Console.ReadLine();
+    int.TryParse(input, out max);
+}
+Console.Write(2);
+int num = 4;
+while (num <= max)
+{
+    Console.Write($",{num}");
+    num += 2;
+}
+Console.WriteLine("");
