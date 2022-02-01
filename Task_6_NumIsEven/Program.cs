dc1409a597568b08a9fb19b2dce4fe8f3ac61678
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-01 12:00:54 +0300

    add solution task 6 is even number

---
+++ Task_6_NumIsEven/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = 0;
+bool isNotInt = true;
+while (isNotInt)
+{
+    Console.Write("Введите целое число: ");
+    string input = Console.ReadLine();
+    isNotInt = !int.TryParse(input, out num);
+}
+Console.WriteLine($"{num} - число " + (num % 2 == 0 ? "четное" : "нечетное"));
