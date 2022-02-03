1a0066714b60d4818bc10c5aff69799ba9772f40
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-03 16:49:12 +0300

    add solution task 17 is squad 2

---
+++ Task_17_IsSquad2/Program.cs
@@ -0,0 +1,29 @@
+﻿// метод ввода числа с проверкой
+double GetDoubleFromConsole(string message)
+{
+    double result = 0;
+    bool isNotCorrect = true;
+    while (isNotCorrect)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        isNotCorrect = !double.TryParse(input, out result);
+    }
+    return result;
+}
+
+Console.WriteLine("Проверим является ли одно введенное целое число квадратом другого");
+double number1 = GetDoubleFromConsole("Введите первое число: ");
+double number2 = GetDoubleFromConsole("Введите второе число: ");
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"{number1} является квадратом {number2}");
+}
+else if (number2 == number1 * number1)
+{
+    Console.WriteLine($"{number2} является квадратом {number1}");
+}
+else
+{
+    Console.WriteLine($"Ни одно из чисел {number1}, {number2} не является квадратом другого.");
+}
