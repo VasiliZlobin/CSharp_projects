cf9e9b8582237d6b603f7a57b2b94016b665c0c8
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-03 16:30:00 +0300

    add solution task 16 holiday

---
+++ Task_16_Holiday/Program.cs
@@ -0,0 +1,26 @@
+﻿// метод ввода целого значения с проверкой
+int GetIntegerFromConsole(string message, (int?, int?) range)
+{
+    int result = 0;
+    bool isNotCorrect = true;
+    while (isNotCorrect)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out result))
+        {
+            isNotCorrect = range.Item1 != null && result < range.Item1 || range.Item2 != null && result > range.Item2;
+        }
+    }
+    return result;
+}
+
+int number = GetIntegerFromConsole("Введите номер дня недели: ", (1, 7));
+if (number > 5)
+{
+    Console.WriteLine("Это выходной день.");
+}
+else
+{
+    Console.WriteLine("Это рабочий день.");
+}
