f5cd3af9aed95af82be42d56936728a49126fd7f
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-03 17:13:39 +0300

    add solution task 18 check statement

---
+++ Task_18_CheckStatement/Program.cs
@@ -0,0 +1,9 @@
+﻿void CheckStatementTwoBool(bool first, bool second)
+{
+    Console.WriteLine($"!({first} || {second}) = {!(first || second)}, !{first} && !{second} = {!first && !second}");
+}
+
+CheckStatementTwoBool(false, false);
+CheckStatementTwoBool(false, true);
+CheckStatementTwoBool(true, false);
+CheckStatementTwoBool(true, true);
