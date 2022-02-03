9650e569cbd33732b00a8a88b4a3313399b49cf3
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-03 13:20:13 +0300

    add solution task 12 delete second digit

---
+++ Task_12_DeleteSecondDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿// метод вычисления цифры в указанной позиции числа (позиция с конца, начинается с 1)
+int GetDigitInPosition(int number, int position)
+{
+    int result = -1;
+    if (position > 0)
+    {
+        int temp = number / (int)Math.Pow(10, position - 1);
+        if (temp > 0)
+        {
+            result = temp % 10;
+        }
+    }
+    return result;
+}
+
+// метод ввода целого значения с проверкой
+int GetIntegerFromConsole(string message, (int?, int?) range)
+{
+    int result = 0;
+    bool isNotCorrect = true;
+    while (isNotCorrect)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out result))
+        {
+            isNotCorrect = range.Item1 != null && result < range.Item1 || range.Item2 != null && result > range.Item2;
+        }
+    }
+    return result;
+}
+
+int number = GetIntegerFromConsole("Введите натуральное трехзначное число: ", (100, 999));
+int lastDigit = GetDigitInPosition(number, 1);
+int firstDigit = GetDigitInPosition(number, 3);
+Console.WriteLine($"{firstDigit}{lastDigit}");
