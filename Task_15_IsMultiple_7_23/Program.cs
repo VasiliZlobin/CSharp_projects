918de148cd32d89e7f407780784d22e70d574490
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-03 16:21:13 +0300

    add solution task 15 num is multiple 7 and 23

---
+++ Task_15_IsMultiple_7_23/Program.cs
@@ -0,0 +1,20 @@
+﻿// метод ввода целого значения с проверкой
+int GetIntegerFromConsole(string message, (int?, int?) range)
+{
+    int result = 0;
+    bool isNotCorrect = true;
+    while (isNotCorrect)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out result))
+        {
+            isNotCorrect = range.Item1 != null && result < range.Item1 || range.Item2 != null && result > range.Item2;
+        }
+    }
+    return result;
+}
+
+int number = GetIntegerFromConsole("Введите целое число: ", (null, null));
+string part = number % 161 == 0 ? "" : " не";
+Console.WriteLine($"{number}{part} кратно 7 и 23.");
