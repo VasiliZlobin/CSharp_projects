da374fcda72bbbfa28a6687e5b8a332eb51acace
Vasili Zlobin <vaszlbn@gmail.com>
2022-02-05 11:44:55 +0300

    add solution task 25 sum from 1 to A

---
+++ Task_25_Sum_1-A/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Задача №25. Найти сумму чисел от 1 до А.
+*/
+
+// метод ввода целого значения с проверкой
+int GetIntegerFromConsole((int?, int?) range)
+{
+    int result = 0;
+    bool isNotCorrect = true;
+    string message = "Введите целое число";
+    if (range.Item1 != null)
+        message += $" от {range.Item1}";
+    if (range.Item2 != null)
+        message += $" по {range.Item2}";
+    message += ": ";
+    while (isNotCorrect)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out result))
+        {
+            isNotCorrect = range.Item1 != null && result < range.Item1 || range.Item2 != null && result > range.Item2;
+        }
+
+    }
+    return result;
+}
+
+int max = GetIntegerFromConsole((2, null));
+double sum = ((double)max * max + max) / 2;
+Console.WriteLine($"Сумма целых чисел от 1 до {max} равна {sum}.");
